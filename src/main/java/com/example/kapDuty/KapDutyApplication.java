e831882b51693d7a7bcc1ef232ed555d54f92343
Aakanksha Bhandari <aakanksha.bhandari@kapturecrm.com>
2025-01-17 16:18:32 +0530

    initial commit

---
+++ src/main/java/com/example/kapDuty/KapDutyApplication.java
@@ -0,0 +1,13 @@
+package com.example.kapDuty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapDutyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapDutyApplication.class, args);
+	}
+
+}
