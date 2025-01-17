9bebf8a9d66f322cb03d706c6ba4775190b08b47
Aakanksha Bhandari <aakanksha.bhandari@kapturecrm.com>
2025-01-17 16:45:45 +0530

    test

--- src/main/java/com/example/kapDuty/controller/Test.java
+++ src/main/java/com/example/kapDuty/controller/Test.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Test {
 
-    @GetMapping("test")
+    @GetMapping("testing")
     public String test() {
         return "Hello world";
     }
